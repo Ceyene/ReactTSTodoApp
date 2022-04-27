64e9357524ee959039515b0821d56aee99fd2687
Cynthia Romero <cyn.jaz.romero@gmail.com>
2022-04-27 12:07:48 -0300

    Using state to complete form handler

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import { useState } from 'react';
 import Todos from './components/Todos';
 import Todo from './models/todo';
 import NewTodo from './components/NewTodo';
 
 function App() {
-	const todos = [new Todo('Learn React'), new Todo('Learn TypeScript')];
+	const [todos, setTodos] = useState<Todo[]>([]); //you need to specify type and a default value
 
-	const addTodoHandler = (todoText: string) => {};
+	const addTodoHandler = (todoText: string) => {
+		const newTodo = new Todo(todoText);
+
+		setTodos((prevTodos) => {
+			return prevTodos.concat(newTodo);
+		});
+	};
 
 	return (
 		<div>
