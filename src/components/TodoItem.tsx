fa325bb577daf9ceda84a13e95b1a27743e763c4
Cynthia Romero <cyn.jaz.romero@gmail.com>
2022-04-27 11:35:07 -0300

    Adding a component and using props

---
+++ src/components/TodoItem.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+//if we don't put a type, its implicitly of type any
+const TodoItem: React.FC<{ text: string }> = (props) => {
+	return <li>{props.text}</li>;
+};
+
+export default TodoItem;
